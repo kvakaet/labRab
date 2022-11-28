d186f0f6613adfcbacd97e8fda9a3879417022fa
Chmonya <towerland2801@gmail.com>
2022-11-28 22:11:52 +0300

    added simple arithmetic operations

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-def readNumbers():
+def read_numbers():
     a, b = map(int, input("введите 2 числа через запятую").split(" "))
     return a, b
 
 
-def readOperation():
+def read_operation():
     operator = input()
     return operator
 
 
+def calc_sum(a, b):
+    return a + b
+
+
+def calc_diff(a, b):
+    return a - b
+
+
+def calc_mul(a, b):
+    return a * b
+
+
+def calc_div(a, b):
+    return a / b
+
+
+def calc_pow(a, b):
+    return a ** b
